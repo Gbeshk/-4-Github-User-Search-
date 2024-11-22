783c733a73b78ad7903f01f6e456f95d0a083959
Gbeshk <giorgibeshkenadze4@gmail.com>
2024-11-23 03:00:09 +0400

    dadw

--- script.js
+++ script.js
@@ -44,14 +44,148 @@ let followers = document.getElementById("followers");
 let followersnumber = document.getElementById("followersnumber");
 let following = document.getElementById("following");
 let followingnumber = document.getElementById("followingnumber");
-let locationicon = document.getElementById("locationicondark");
-let blogicon = document.getElementById("blogicondark");
-let buildingicon = document.getElementById("buildingicondark");
+let locationicon = document.getElementById("iconlocation");
+let darklocationicon = document.getElementById("locationicondark");
 let locationn = document.getElementById("locationn");
 let blogg = document.getElementById("blogg");
-let build = document.getElementById("build");
-let twitter = document.getElementById("twitter1");
-let twittericon = document.getElementById("darktwittericon1");
+let blogicon = document.getElementById("iconblog");
+let darkblogicon = document.getElementById("blogicondark");
+let companyicon = document.getElementById("iconbuilding");
+let darkcompanyicon = document.getElementById("buildingicondark");
+let company1 = document.getElementById("build");
+let twitter1 = document.getElementById("twitter1");
+let twitericon = document.getElementById("icontwitter");
+let darktwittericon = document.getElementById("darktwittericon1");
+let form = document.getElementById("forma");
+let serch = document.getElementById("search");
+
+let datee = document.getElementById("datee");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  async function getAllProducts() {
+    try {
+      let data = await fetch(`https://api.github.com/users/${input.value}`);
+      let ouruser = await data.json();
+      console.log(ouruser);
+      let img = document.getElementById("pic");
+      img.src = ouruser.avatar_url;
+      let userdate = ouruser.created_at;
+
+      datee.textContent = userdate;
+      let name1 = document.getElementById("user");
+      name1.textContent = ouruser.name;
+      let username1 = document.getElementById("nameuser");
+      username1.textContent = `@${ouruser.login}`;
+      let following1 = document.getElementById("followingnumber");
+      following1.textContent = ouruser.following;
+      let followers1 = document.getElementById("followersnumber");
+      followers1.textContent = ouruser.followers;
+      let repos1 = document.getElementById("numberrepos");
+      repos1.textContent = ouruser.public_repos;
+      let bio1 = document.getElementById("biio");
+      if (ouruser.location) {
+        locationn.textContent = ouruser.location;
+        locationn.style.opacity = 1;
+        if (locationn.className == "location") {
+          locationicon.style.opacity = 1;
+        } else {
+          darklocationicon.style.opacity = 1;
+        }
+      } else {
+        locationn.textContent = "Not Avaliable";
+        locationn.style.opacity = 0.5;
+        if (locationn.className == "location") {
+          locationicon.style.opacity = 0.5;
+        } else {
+          darklocationicon.style.opacity = 0.5;
+        }
+      }
+      if (ouruser.company) {
+        company1.textContent = ouruser.company;
+        company1.style.opacity = 1;
+        if (company1.className == "building") {
+          companyicon.style.opacity = 1;
+        } else {
+          darkcompanyicon.style.opacity = 1;
+        }
+      } else {
+        company1.textContent = "Not Avaliable";
+        company1.style.opacity = 0.5;
+        if (company1.className == "location") {
+          companyicon.style.opacity = 0.5;
+        } else {
+          darkcompanyicon.style.opacity = 0.5;
+        }
+      }
+      if (ouruser.blog) {
+        blogg.textContent = ouruser.blog;
+        blogg.style.opacity = 1;
+        if (blogg.className == "blog") {
+          blogicon.style.opacity = 1;
+        } else {
+          darkblogicon.style.opacity = 1;
+        }
+      } else {
+        blogg.textContent = "Not Avaliable";
+        blogg.style.opacity = 0.5;
+        if (blogg.className == "blog") {
+          blogicon.style.opacity = 0.5;
+        } else {
+          darkblogicon.style.opacity = 0.5;
+        }
+      }
+      if (ouruser.bio) {
+        bio1.textContent = ouruser.bio;
+      }
+      if (ouruser.bio && ouruser.bio.length > 100) {
+        bio1.style.fontSize = "12px";
+      }
+      if (ouruser.company.length > 40) {
+        company1.style.fontSize = "12px";
+      }
+
+      if (ouruser.blog && ouruser.blog.length > 0) {
+        blogg.textContent = ouruser.blog;
+        blogg.style.opacity = 1;
+        if (blogg.className == "blog") {
+          blogicon.style.opacity = 1;
+        } else {
+          darkblogicon.style.opacity = 1;
+        }
+      } else {
+        blogg.textContent = "Not Avaliable";
+        blogg.style.opacity = 0.5;
+        if (blogg.className == "blog") {
+          blogicon.style.opacity = 0.5;
+        } else {
+          darkblogicon.style.opacity = 0.5;
+        }
+      }
+      if (ouruser.twitter_username) {
+        twitter1.style.opacity = 1;
+        twitter1.textContent = ouruser.twitter_username;
+        if (twitter1.className == "twitter") {
+          twitericon.style.opacity = 1;
+        } else {
+          darktwittericon.style.opacity = 1;
+        }
+      } else {
+        twitter1.textContent = "Not Avaliable";
+        twitter1.style.opacity = 0.5;
+        if (twitter1.className == "twitter") {
+          twitericon.style.opacity = 0.5;
+        } else {
+          darktwittericon.style.opacity = 0.5;
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    input.value = "";
+  }
+  getAllProducts();
+});
 
 let x = 0;
 if (localStorage.getItem("task") % 2 == 1) {
@@ -60,6 +194,40 @@ if (localStorage.getItem("task") % 2 == 1) {
 btn.addEventListener("click", () => {
   x++;
   localStorage.setItem("task", x);
+  locationicon.style.opacity = 0;
+  if (locationn.textContent == "Not Avaliable") {
+    darklocationicon.style.opacity = 0.5;
+  } else {
+    darklocationicon.style.opacity = 1;
+  }
+  companyicon.style.opacity = 0;
+  if (company1.textContent == "Not Avaliable") {
+    darkcompanyicon.style.opacity = 0.5;
+  } else {
+    darkcompanyicon.style.opacity = 1;
+  }
+  blogicon.style.opacity = 0;
+  if (blogg.textContent == "Not Avaliable") {
+    darkblogicon.style.opacity = 0.5;
+  } else {
+    darkblogicon.style.opacity = 1;
+  }
+  twitericon.style.opacity = 0;
+  if (twitter1.textContent == "Not Avaliable") {
+    darktwittericon.style.opacity = 0.5;
+  } else {
+    darktwittericon.style.opacity = 1;
+  }
+  twitericon.style.opacity = 0;
+  if (twitter1.textContent == "Not Avaliable") {
+    darktwittericon.style.opacity = 0.5;
+  } else {
+    darktwittericon.style.opacity = 1;
+  }
+  twitter1.className = "darktwitter";
+  blogg.className = "darkblog";
+  company1.className = "darkbuilding";
+  locationn.className = "darklocation";
   boddy.className = "darkbody";
   headerh1.className = "darkheaderh1";
   darkbtn.style.opacity = 1;
@@ -78,18 +246,48 @@ btn.addEventListener("click", () => {
   followersnumber.className = "darkreposnumber";
   following.className = "darkrepos";
   followingnumber.className = "darkreposnumber";
-  locationicon.style.opacity = 1;
-  blogicon.style.opacity = 1;
-  buildingicon.style.opacity = 1;
-  locationn.className = "darklocation";
-  blogg.className = "darkblog";
-  build.className = "darkbuilding";
   darkbtn.style.transition = "none";
 });
 
 darkbtn.addEventListener("click", () => {
   x++;
   localStorage.setItem("task", x);
+  if (locationn.textContent == "Not Avaliable") {
+    locationicon.style.opacity = 0.5;
+  } else {
+    locationicon.style.opacity = 1;
+  }
+  darklocationicon.style.opacity = 0;
+  darkcompanyicon.style.opacity = 0;
+  if (company1.textContent == "Not Avaliable") {
+    companyicon.style.opacity = 0.5;
+  } else {
+    companyicon.style.opacity = 1;
+  }
+  darkblogicon.style.opacity = 0;
+  if (blogg.textContent == "Not Avaliable") {
+    blogicon.style.opacity = 0.5;
+  } else {
+    blogicon.style.opacity = 1;
+  }
+  company1.className = "building";
+  blogg.className = "blog";
+  locationn.className = "location";
+
+  darktwittericon.style.opacity = 0;
+  if (twitter1.textContent == "Not Avaliable") {
+    twitericon.style.opacity = 0.5;
+  } else {
+    twitericon.style.opacity = 1;
+  }
+  twitericon.style.opacity = 0;
+  if (twitter1.textContent == "Not Avaliable") {
+    darktwittericon.style.opacity = 0.5;
+  } else {
+    darktwittericon.style.opacity = 1;
+  }
+  twitter1.className = "darktwitter";
+
   boddy.className = "body";
   headerh1.className = "headerh1";
   darkbtn.style.opacity = 0;
@@ -109,16 +307,31 @@ darkbtn.addEventListener("click", () => {
   followersnumber.className = "reposnumber";
   following.className = "repos";
   followingnumber.className = "reposnumber";
-  locationicon.style.opacity = 0;
-  blogicon.style.opacity = 0;
-  buildingicon.style.opacity = 0;
-  locationn.className = "location";
-  blogg.className = "blog";
-  build.className = "building";
 });
 
 let y = localStorage.getItem("task");
 if (y % 2 == 0) {
+  if (locationn.textContent == "Not Avaliable") {
+    locationicon.style.opacity = 0.5;
+  } else {
+    locationicon.style.opacity = 1;
+  }
+  darklocationicon.style.opacity = 0;
+  darkcompanyicon.style.opacity = 0;
+  if (company1.textContent == "Not Avaliable") {
+    companyicon.style.opacity = 0.5;
+  } else {
+    companyicon.style.opacity = 1;
+  }
+  darkblogicon.style.opacity = 0;
+  if (blogg.textContent == "Not Avaliable") {
+    blogicon.style.opacity = 0.5;
+  } else {
+    blogicon.style.opacity = 1;
+  }
+  company1.className = "building";
+  blogg.className = "blog";
+  locationn.className = "location";
   boddy.className = "body";
   headerh1.className = "headerh1";
   darkbtn.style.opacity = 0;
@@ -138,14 +351,29 @@ if (y % 2 == 0) {
   followersnumber.className = "reposnumber";
   following.className = "repos";
   followingnumber.className = "reposnumber";
-  locationicon.style.opacity = 0;
-  blogicon.style.opacity = 0;
-  buildingicon.style.opacity = 0;
-  locationn.className = "location";
-  blogg.className = "blog";
-  build.className = "building";
 }
 if (y % 2 == 1) {
+  locationicon.style.opacity = 0;
+  if (locationn.textContent == "Not Avaliable") {
+    darklocationicon.style.opacity = 0.5;
+  } else {
+    darklocationicon.style.opacity = 1;
+  }
+  companyicon.style.opacity = 0;
+  if (company1.textContent == "Not Avaliable") {
+    darkcompanyicon.style.opacity = 0.5;
+  } else {
+    darkcompanyicon.style.opacity = 1;
+  }
+  blogicon.style.opacity = 0;
+  if (blogg.textContent == "Not Avaliable") {
+    darkblogicon.style.opacity = 0.5;
+  } else {
+    darkblogicon.style.opacity = 1;
+  }
+  blogg.className = "darkblog";
+  company1.className = "darkbuilding";
+  locationn.className = "darklocation";
   boddy.className = "darkbody";
   headerh1.className = "darkheaderh1";
   darkbtn.style.opacity = 1;
@@ -164,12 +392,7 @@ if (y % 2 == 1) {
   followersnumber.className = "darkreposnumber";
   following.className = "darkrepos";
   followingnumber.className = "darkreposnumber";
-  locationicon.style.opacity = 1;
-  blogicon.style.opacity = 1;
-  buildingicon.style.opacity = 1;
-  locationn.className = "darklocation";
-  blogg.className = "darkblog";
-  build.className = "darkbuilding";
+  darkbtn.style.transition = "none";
 }
 darkbtn.addEventListener("mouseover", () => {
   darkbtn.style.color = "#90A4D4";
@@ -186,78 +409,4 @@ darkbtn.addEventListener("mouseleave", () => {
   icon12.style.opacity = 0;
   icon11.style.transition = "1.5s";
   icon12.style.transition = "1.5s";
-});
-
-let form = document.getElementById("forma");
-let serch = document.getElementById("search");
-
-function fun(a, b) {}
-
-form.addEventListener("submit", (e) => {
-  async function getAllProducts() {
-    e.preventDefault();
-    try {
-      let data = await fetch(`https://api.github.com/users/${input.value}`);
-      let ouruser = await data.json();
-      console.log(ouruser);
-      let name1 = document.getElementById("user");
-      name1.textContent = ouruser.name;
-      let username1 = document.getElementById("nameuser");
-      username1.textContent = `@${ouruser.login}`;
-      let date1 = document.getElementById("datee");
-      // datee1.textContent = `joined ${ouruser.login[8]${ouruser.login[9]}}`
-      let following1 = document.getElementById("followingnumber");
-      following1.textContent = ouruser.following;
-      let followers1 = document.getElementById("followersnumber");
-      followers1.textContent = ouruser.followers;
-      let location1 = document.getElementById("locationn");
-      location1.textContent = ouruser.location;
-      if (
-        ouruser.location == null ||
-        ouruser.location == undefined ||
-        ouruser.location.length < 1
-      ) {
-        location1.textContent = "Not Avaliable";
-        location1.style.opacity = 0.5;
-        let iconlocation = document.getElementById("iconlocation");
-        iconlocation.textContent = "Not Avaliable";
-        iconlocation.style.opacity = 0.5;
-      }
-      let repos1 = document.getElementById("numberrepos");
-      repos1.textContent = ouruser.public_repos;
-      let blog1 = document.getElementById("blogg");
-      blog1.textContent = ouruser.blog;
-      if (
-        ouruser.blog == null ||
-        ouruser.blog == undefined ||
-        ouruser.blog.length < 1
-      ) {
-        blog1.textContent = "Not Avaliable";
-        blog1.style.opacity = 0.5;
-        let iconblog = document.getElementById("iconblog");
-        iconblog.textContent = "Not Avaliable";
-        iconblog.style.opacity = 0.5;
-      }
-      let bio1 = document.getElementById("biio");
-      if (ouruser.bio != null) {
-        bio1.textContent = ouruser.bio;
-      }
-      let company = document.getElementById("build");
-      if (ouruser.company.length > 40) {
-        company.style.fontSize = "12px";
-      }
-      company.textContent = ouruser.company;
-      if (ouruser.twitter_username.length) {
-        let twitter1 = document.getElementById("twitter1");
-        twitter1.style.opacity = 1;
-        let icontwitter = document.getElementById("icontwitter");
-        icontwitter.style.opacity = 1;
-        twitter1.textContent = ouruser.twitter_username;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-    input.value = " ";
-  }
-  getAllProducts();
 });
